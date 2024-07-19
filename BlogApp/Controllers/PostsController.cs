0bbc853e21725c67dbcca57c3565aa4922740cb9
BorayGulpinar <boray112@gmail.com>
2024-07-19 21:48:44 +0300

    commit Tags

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -1,21 +1,30 @@
 ﻿using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.AspNetCore.Mvc;
+using BlogApp.Models;
 
 namespace BlogApp.Controllers
 {
     public class PostsController : Controller
     {
         private IPostRepository _postRepository;
+        private ITagRepository _tagRepository;
 
-        public PostsController(IPostRepository postRepository)
+        public PostsController(IPostRepository postRepository, ITagRepository tagRepository)
         {
             _postRepository = postRepository;
+            _tagRepository = tagRepository;
         }
 
         public IActionResult Index()
         {
-            return View(_postRepository.Posts.ToList());
+            return View(
+                new PostWithTagViewModel
+                {
+                    Posts = _postRepository.Posts.ToList(),
+                    Tags = _tagRepository.Tags.ToList()
+                }
+                );
         }
     }
 }
---
+++ BlogApp/Models/PostWithTagViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Entitiy;
+
+namespace BlogApp.Models
+{
+    public class PostWithTagViewModel
+    {
+        public List<Post> Posts { get; set; } = new();
+        public List<Tag> Tags { get; set; } = new();
+    }
+}
